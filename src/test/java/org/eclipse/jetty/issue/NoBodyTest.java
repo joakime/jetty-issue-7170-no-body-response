dbfe0d4f9534f1fea21911da8cd37b599a58414a
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2021-11-29 10:12:40 -0600

    Using Jetty 9.4.44 and Servlet 3.1

--- src/test/java/org/eclipse/jetty/issue/NoBodyTest.java
+++ src/test/java/org/eclipse/jetty/issue/NoBodyTest.java
@@ -46,6 +46,7 @@ public class NoBodyTest
         }
 
         WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
         webapp.setBaseResource(new PathResource(webappPath));
         webapp.setExtraClasspath("target/classes");
 
@@ -59,6 +60,7 @@ public class NoBodyTest
     @AfterEach
     public void teardownServer()
     {
+        System.err.println("### teardownServer()");
         LifeCycle.stop(server);
     }
 
@@ -67,8 +69,10 @@ public class NoBodyTest
     {
         HttpClient httpClient = HttpClient.newHttpClient();
         URI uri = server.getURI().resolve("/nobody");
-        HttpRequest httpRequest = HttpRequest.newBuilder(uri).GET().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder(uri).DELETE().build();
         HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        String contentLengthResponseHeader = httpResponse.headers().firstValue("Content-Length").orElse("<null>");
+        System.out.printf("Response Body (Content-Length: %s): [%s]%n", contentLengthResponseHeader, httpResponse.body());
         assertEquals(501, httpResponse.statusCode(), "Response status code");
     }
 }
